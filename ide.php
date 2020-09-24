6501d859ff5855a2c678d632295ca6246c120d6b
nitk2038 <nitk17ec@cmrit.ac.in>
2020-09-24 22:04:21 +0530

    raju rastogi

--- ide.php
+++ ide.php
@@ -2,9 +2,7 @@
 <head>
 	<title>ide2</title>
 	<link rel="stylesheet" href="css/style.css" type="text/css">
-
      <title>ide2</title>
-
 	<!-- tab img -->
 	<link rel="shortcut icon" type="image/x-icon" href="assets/img/ide2.png">
     
@@ -14,15 +12,12 @@
 	<link rel="stylesheet" href="assets/css/magnific-popup.css">
 	<link rel="stylesheet" href="assets/css/flexslider.css">
 	<link rel="stylesheet" href="assets/css/aos.css">
-
 	<!-- main css ------------------------------------------------------------------------------------------------------------------------------------------------- -->
 	<link rel="stylesheet" href="assets/css/style.css">
 	<link rel="stylesheet" href="assets/css/style-responsives.css">
-
 	<!-- google fonts ---------------------------------------------------------------------------------------------------------------------------------------------- -->
 	<link href="https://fonts.googleapis.com/css?family=Hind:600|Lato:300,400|Ubuntu" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-
 <style>
 #button1 , #button2
  {
@@ -31,15 +26,11 @@ display:inline-block;
 }
 </style>
 </head>
-
 <body>
 <ul class="breadcrumb">
 	<li><a href="index.php">Home</a></li>
 	<li><a href="rank.php">Rank</a></li>
 </ul>	
-
-
-
 <form>
   <div class="form-group col-sm-7 col-xs-12 inputDiv">
     <select style="width: 18em; display:inline;" name="programTypeId">
@@ -51,13 +42,11 @@ display:inline-block;
         <option value="6">Python 3.7.2</option>
     </select>
   </div>
-
   <div class="form-group col-sm-7 col-xs-12 inputDiv">
      <textarea id="myInput" placeholder="write your input" maxlength="10000" class="gb wf form-control input" style="height:200px;"></textarea>
      <button onclick="myFunction()">Copy text</button>
     <input type="file" class="form-control-file" id="button2">
 </div>
-
 <script>
 function myFunction() {
   var copyText = document.getElementById("myInput");
@@ -67,7 +56,4 @@ function myFunction() {
   alert("Copied the text: " + copyText.value);
 }
 </script>
-
-
 </form>
-
--- index.php
+++ index.php
@@ -12,19 +12,15 @@
 	<link rel="stylesheet" href="assets/css/magnific-popup.css">
 	<link rel="stylesheet" href="assets/css/flexslider.css">
 	<link rel="stylesheet" href="assets/css/aos.css">
-
-
     
-
 	<!-- main css ------------------------------------------------------------------------------------------------------------------------------------------------- -->
 	<link rel="stylesheet" href="assets/css/style.css">
 	<link rel="stylesheet" href="assets/css/style-responsives.css">
-
 	<!-- google fonts ---------------------------------------------------------------------------------------------------------------------------------------------- -->
 	<link href="https://fonts.googleapis.com/css?family=Hind:600|Lato:300,400|Ubuntu" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-
 	<style> 
+
 .count-item
  {
   -webkit-flex: 3;
@@ -33,33 +29,28 @@
   background-color: #f1f1f1;
   padding: 10px;
 }
-
         /* Modify the background color */ 
           
         .navbar-custom { 
             background-color: black; 
         } 
-
-
 #footer {
   position: absolute;
   bottom: 0;
   width: 100%;
   
 }
-
 body {
  
   overflow: hidden; /* Hide scrollbars */
+ 
 }
-
 .card{
 		width: 700px;
 		height: 200px;
 		
 	
 	}
-
 .item-1, 
 .item-2, 
 .item-3 {
@@ -70,51 +61,47 @@ body {
   width: 60%;
   
   font-size: 2em;
-
 	-webkit-animation-duration: 20s;
-
 	        animation-duration: 20s;
 	-webkit-animation-timing-function: ease-in-out;
 	        animation-timing-function: ease-in-out;
 	-webkit-animation-iteration-count: infinite;
 	        animation-iteration-count: infinite;
 }
-
 .item-1{
 	-webkit-animation-name: anim-1;
 	        animation-name: anim-1;
 }
-
 .item-2{
 	-webkit-animation-name: anim-2;
 	        animation-name: anim-2;
 }
-
 .item-3{
 	-webkit-animation-name: anim-3;
 	        animation-name: anim-3;
 }
-
 img{
-  height:200px;
+  height:150px;
   width:100%;
 }
-
-.item{
-  padding-left:10px;
-  padding-right:10px;
+.card {
+    width: 100%;
+    height: 200px;
+    padding: 20px;
+    background-color:lightgray;
 }
+/* .item{
+  padding-left:5px;
+  padding-right:5px;
+} */
 .item-card{
   transition:0.5s;
   cursor:pointer;
 }
-
 .card:hover{
   transform: scale(1.05);
   box-shadow: 10px 10px 15px rgba(0,0,0,0.3);
 }
-
-
 .card::before, .card::after {
   position: absolute;
   top: 0;
@@ -123,6 +110,7 @@ img{
   left: 0;
   transform: scale3d(0, 0, 1);
   transition: transform .3s ease-out 0s;
+  background: rgba(255, 255, 255, 0.1);
   content: '';
   pointer-events: none;
 }
@@ -135,46 +123,38 @@ img{
 .card:hover::before, .card:hover::after, .card:focus::before, .card:focus::after {
   transform: scale3d(1, 1, 1);
 }
-
 @-webkit-keyframes anim-1 {
 	0%, 8.3% { left: -100%; opacity: 0; }
   8.3%,25% { left: 25%; opacity: 1; }
   33.33%, 100% { left: 110%; opacity: 0; }
 }
-
 @keyframes anim-1 {
 	0%, 8.3% { left: -100%; opacity: 0; }
   8.3%,25% { left: 25%; opacity: 1; }
   33.33%, 100% { left: 110%; opacity: 0; }
 }
-
 @-webkit-keyframes anim-2 {
 	0%, 33.33% { left: -100%; opacity: 0; }
   41.63%, 58.29% { left: 25%; opacity: 1; }
   66.66%, 100% { left: 110%; opacity: 0; }
 }
-
 @keyframes anim-2 {
 	0%, 33.33% { left: -100%; opacity: 0; }
   41.63%, 58.29% { left: 25%; opacity: 1; }
   66.66%, 100% { left: 110%; opacity: 0; }
 }
-
 @-webkit-keyframes anim-3 {
 	0%, 66.66% { left: -100%; opacity: 0; }
   74.96%, 91.62% { left: 25%; opacity: 1; }
   100% { left: 110%; opacity: 0; }
 }
-
 @keyframes anim-3 {
 	0%, 66.66% { left: -100%; opacity: 0; }
   74.96%, 91.62% { left: 25%; opacity: 1; }
   100% { left: 110%; opacity: 0; }
 }
     </style> 
-
 </head>
-
 <body>
 <nav class="navbar navbar-custom"> 
         <a class="navbar-brand" href="#"></a> 
@@ -188,7 +168,6 @@ img{
 						<a href="index.php"> ide2</a>
 					</div>
 				</div>
-
 				<div class="col-sm-9">
 					<div class="navigation-menu">
 						<div class="navbar">
@@ -200,12 +179,11 @@ img{
 									<span class="icon-bar"></span>
 								</button>
 							</div>
-
 							<div class="navbar-collapse collapse">
 								<ul class="nav navbar-nav navbar-right">
 									<li><a class="active smoth-scroll" href="#particles-js">Home</a></li>
 									<li><a class="smoth-scroll" href="ide.php">Ide</a></li>
-									<li><a class="smoth-scroll" href="#">Signup</a></li>
+									<li><a class="smoth-scroll" href="signup.php">Signup</a></li>
 									<li><a class="smoth-scroll" href="#">Login</a></li>
 									<li><a class="smoth-scroll" href="Contact.php">Contact</a></li>
 								</ul>
@@ -217,14 +195,11 @@ img{
 		</div>
 	</div>
 	<!-- // NAVIGATION END ---------------------------------------------------------------------------------------------------------------------------------------------->
-
 <body translate="no">
 <br></br>
     <p class="item-1">	Codeforces Round #672 (Div. 2) &nbsp Sep/24/2020   &nbsp 20:05UTC+5.5</p>
 	<p class="item-2">  <a href="https://discuss.codechef.com/g/CodeChef-DSA-Learners-Group">https://discuss.codechef.com/g/CodeChef-DSA-Learners-Group</a></p>
 	<p class="item-3">
-
-
 <br></br>
 <br></br>
 <br></br>
@@ -234,7 +209,6 @@ img{
 				<div class="col-md-12 text-center">
 					<br><h2>Courses</h2></br>
 				</div>
-
 				<!----1st image---->
 <!-- 
 				<div class="col-md-4 col-sm-6">
@@ -245,9 +219,7 @@ img{
 						
 					</div>
 				</div> -->
-
 				<!---2nd image ------->
-
 				<!-- <div class="col-md-4 col-sm-6">
 					<div class="blog-item">
 						<a   class="blog-img">
@@ -256,9 +228,7 @@ img{
 						
 					</div>
 				</div> -->
-
 				<!--- 3rd image------>
-
 				<!-- <div class="col-md-4 col-sm-6">
 					<div class="blog-item">
 						<a   class="blog-img">
@@ -267,45 +237,40 @@ img{
 						
 					</div>
 				</div> -->
-
 				
-
 			</div>
 		</div>
 	</section> 
-
 	<div class="container mt-2">
-		<div class="row">
-    		<div class="col-md-4 col-sm-6 item">
-      			<div class="card item-card card-block">
-    				<img src="assets/img/code1.jpeg"  alt="code1">
+  <div class="row">
+    <div class="col-md-4 col-sm-6 item">
+      <div class="card item-card card-block">
+      
+    <img src="assets/img/code1.jpeg" alt="code1">
         
-  				</div>
-			</div>
-			<div class="col-md-4 col-sm-6 item">
-      			<div class="card item-card card-block">
-    				<img src="assets/img/code2.png"  alt="code2">
+  </div>
+    </div>
+    <div class="col-md-4 col-sm-6 item">
+      <div class="card item-card card-block">
+      
+    <img src="assets/img/code2.png" alt="code2">
+         
+  </div>
+    </div>
+    <div class="col-md-4 col-sm-6 item">
+      <div class="card item-card card-block">
+      
+    <img src="assets/img/code3.jpeg" alt="code3">
         
-  				</div>
-			</div>
-			<div class="col-md-4 col-sm-6 item">
-      			<div class="card item-card card-block">
-    				<img src="assets/img/code3.jpeg"  alt="code3">
-        
-  				</div>
-			</div>
-		</div>
-	</div>
-
+  </div>
+    </div>
+       
+  </div>
+  
+</div>
 			
-
-
 <br></br>
-
-
 <!--------------- welcome ide2 --->
-
-
 	<div class="description col-md-6 col-sm-4 col-xs-12">
 		<h2> Welcome  <strong style="color: #4cd137"> IDE2</strong></h2>
 			<h4> No.1 coding platform !!</h4>
@@ -313,25 +278,18 @@ img{
 						<br>
 					</p>			 
 	</div>
-
-
 	<div class="col-md-6 col-sm-6 col-xs-6">
 							<div class="count-item">
 								<div class="numscroller roller-title-number-2 scrollzip isShown" data-slno="2" data-min="0" data-max="12" data-delay="6" data-increment="1">3</div>
-								<div class="count-name-intro">Courses Available  "<i class="fa fa-youtube-play"></i></div>
+								<div class="count-name-intro">Courses Available  <i class="fa fa-youtube-play"></i></div>
 							</div>
-	</div>
-
-
-<script src="particles.js"></script>
-<script src="app.js"></script>
+						</div> 
+    <script src="particles.js"></script>
+    <script src="app.js"></script>
 </body>
-
-
 <footer id="footer">
 		<div class="container">
 			<div class="col-md-12 text-center">
-
 				<h3> follow us ;)</h3>
 				<ul class="social-icons-footer">
 				
@@ -343,7 +301,5 @@ img{
 		</div>
 	</footer>		
 			
-
-
  
 </html>
